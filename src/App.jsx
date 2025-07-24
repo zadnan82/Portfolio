a1d2b69ba004a4f7ca00ac4a9ae493bed43b2816
zadnan82 <zadnaniths@gmail.com>
2025-07-24 21:29:02 +0200

    added term and policies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-import Portfolio from './components/Portfolio'
- 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Portfolio from './components/Portfolio';  
+import FoodPrivacy from './components/docs/FoodPrivacy';
+import FoodTerms from './components/docs/FoodTerms';
 
 function App() {
-  return <Portfolio />
+  return (
+    <Router>
+      <Routes>
+        {/* Main Portfolio (unchanged) */}
+        <Route path="/" element={
+          <Layout currentPageName="Portfolio">
+            <Portfolio />
+          </Layout>
+        } />
+        
+        {/* Hidden Pages (same layout but no nav) */}
+        <Route path="/food-privacy" element={
+          <Layout currentPageName="Privacy Policy">
+            <FoodPrivacy />
+          </Layout>
+        } />
+        
+        <Route path="/food-terms" element={
+          <Layout currentPageName="Terms of Service">
+            <FoodTerms />
+          </Layout>
+        } />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
